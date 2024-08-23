27ab309036674326704d094e9725c3d4ebbc231b
michtruksa <michtruksa@gmail.com>
2024-08-23 13:59:30 +0200

    fix

--- src/router.tsx
+++ src/router.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TodoList from "./todoList";
 
 const Router = () => {
-  console.log("here");
   return (
     <BrowserRouter>
       <Routes>
