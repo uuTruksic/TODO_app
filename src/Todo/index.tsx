a6c7f69a740c6f24cdfaffeb3e2083b99e891d63
David Tauchen <davidtauchen.cz@seznam.cz>
2024-09-06 12:13:22 +0200

    gg

--- src/Todo/index.tsx
+++ src/Todo/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import TodoAdder from "./todoAdder";
 import TodoList from "./todoList";
 
@@ -9,14 +9,39 @@ export interface TodoInterface {
 }
 
 const Todo = () => {
-  const [todoList, setTodoList] = useState<TodoInterface[]>([
+  const todoDefaultValues = [
     { id: 0, value: "ahoj", checked: true },
     { id: 1, value: "nazdar", checked: true },
     { id: 2, value: "cau", checked: true },
-  ]);
+  ]
+
+  
+  const [todoList, setTodoList] = useState<TodoInterface[]>(todoDefaultValues);
+  const todoLoadedLocalStorage = useRef(false)
+
+  useEffect(() => {
+    if (localStorage.getItem("todoData") === null) {
+      localStorage.setItem("todoData", JSON.stringify(todoList))
+    } else {
+      const localStorageData = localStorage.getItem("todoData")
+      if (localStorageData) {
+        setTodoList(JSON.parse(localStorageData))
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    if(!todoLoadedLocalStorage.current){
+      todoLoadedLocalStorage.current = true
+      return
+    }
+
+    localStorage.setItem("todoData", JSON.stringify(todoList))
+
+  }, [todoList])
 
   return (
-    <div className="w-screen min-h-screen p-8 bg-yellow-50">
+    <div className="p-8 w-screen min-h-screen bg-yellow-50">
       <TodoAdder setTodoList={setTodoList} />
       <TodoList todoList={todoList} setTodoList={setTodoList} />
     </div>
